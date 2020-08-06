49bd0adb8acd5650577aa8900d758a5d10b38047
purfeld <wojo111115@wp.pl>
2020-08-06 18:50:24 +0200

    c#_kurs

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // Stan (zmienne - pola)
+        List<float> ratings = new List<float>();
+
+        // Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float Average()
+        {
+            float sum = 0;
+            float avg = 0;
+            foreach (var rate in ratings)
+	        {
+                sum += rate;
+	        }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        public float MaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float MinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+
+            //float avg = diary.Average();
+            //float max = diary.MaxRating();
+            //float min = diary.MinRating();
+
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocenę: ");
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+
+                if (rating == 555)
+                {
+                    break;
+                }
+
+                if (result) // = result == true
+                {
+                    if (rating > 0 && rating <= 10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna liczba! Podaj liczbę z zakresu 1 - 10");
+                    }
+                    
+                }
+
+            }
+
+            Console.WriteLine("Średnia ocen to: " + diary.Average());
+            Console.WriteLine("Najwyższa ocena to: " + diary.MaxRating());
+            Console.WriteLine("Najniższa ocena to: " + diary.MinRating());
+            Console.ReadKey();
+
+            Diary diary2 = new Diary();
+            Diary diary3 = new Diary();
+
+        }
+    }
+}
