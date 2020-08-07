0aabebb92e623494a287371c00932df343e7cd32
purfeld <wojo111115@wp.pl>
2020-08-07 18:01:51 +0200

    2.3

---
+++ Constructrors/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructrors
+{
+    class Person
+    {
+        int id;
+        string firstname;
+        string lastname;
+        string address;
+        int age;
+        string gender;
+
+        public Person()
+        {
+            id = 1;
+            firstname = "Piotr";
+            lastname = "Granger";
+            age = 14;
+        }
+
+        public Person(int id, string firstname, string lastname, string address, int age, string gender)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.age = age;
+            this.gender = gender;
+        }
+        public Person(int id, string firstname, string lastname)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            address = "Warszawa";
+            age = 20;
+        }
+        public Person(int id, string firstname)
+        {
+            this.id = id;
+            this.firstname = firstname;
+        }
+    }
+}
---
+++ Constructrors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person2 = new Person(1, "Tomasz");
+            Person person3 = new Person(2, "Michał", "Ptak");
+            Person person4 = new Person(3, "Adam", "Kot", "Szczecin", 22, "Male");
+
+        }
+    }
+}
